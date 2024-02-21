6bb36cc546c484f67d7fcddc06053034c365d380
marioBytes <mario.bytes92@gmail.com>
2024-02-21 11:36:47 -0700

    Sample data

---
+++ src/assets/data/projects.js
@@ -0,0 +1,13 @@
+import devfinder from '../images/projects/devfinder.png';
+
+const projects = [
+  {
+    id: 1,
+    name: 'devfinder',
+    imgSource: devfinder,
+    description: 'This app is used to search for users on GitHub using their open API. The UI has a light and dark mode which is set by default depending on the user\'s system settings. The app was built using React with assistance from other libraries like Axios for fetch requests and React Loading Skeleton for the loading screen.',
+    links: [{ id: 1, source: 'Github', link: 'https://github.com/marioBytes' }],
+  },
+];
+
+export default projects;
