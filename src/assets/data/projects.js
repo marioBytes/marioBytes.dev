1426003fb9ad55bde3e25a490a967054572c7d06
marioBytes <mario.bytes92@gmail.com>
2024-02-22 14:51:10 -0700

    Update project description

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     id: 1,
     name: 'devfinder',
     imgSource: devfinder,
-    description: 'This app is used to search for users on GitHub using their open API. The UI has a light and dark mode which is set by default depending on the user\'s system settings. The app was built using React with assistance from other libraries like Axios for fetch requests and React Loading Skeleton for the loading screen.',
+    description: 'This application facilitates the search for GitHub users utilizing their open API. It features a user interface that automatically switches between light and dark modes based on the system settings of the user. Developed with React, the app also incorporates additional libraries such as Axios for handling fetch requests and React Loading Skeleton to enhance the loading screen experience.',
     links: [{ id: 1, source: 'Github', link: 'https://github.com/marioBytes' }],
   },
 ];
