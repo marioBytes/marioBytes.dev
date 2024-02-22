97db1f98762be404d120e1b585b6b9f32f8abf02
marioBytes <mario.bytes92@gmail.com>
2024-02-22 13:48:02 -0700

    Add work experience

---
+++ src/assets/data/resume.js
@@ -0,0 +1,86 @@
+export const resume = [
+  {
+    id: 5,
+    companyName: 'Carelon Health',
+    companyUrl: 'https://www.carelon.com/',
+    jobPosition: 'Full-Stack Developer',
+    startDate: new Date('October 1, 2022'),
+    endDate: new Date('February 9, 2024'),
+    responsibilities: [
+      {
+        description:
+          'Developed API for importing external patient data, thus automating manual processes and enhancing efficiency for front-end users.',
+      },
+      {
+        description:
+          "Collaborated with SA's and business for accurate, timely project completion, ensuring effective teamwork and seamless processes.",
+      },
+      { description: 'Developed scripts to eliminate any erroneous data from our database.' },
+    ],
+    technologies_used: 'Phoenix, Elixir, JavaScript, React, Docker, PostgreSQL, JSON, Git, BitBucket, and JIRA',
+  },
+  {
+    id: 4,
+    companyName: 'Xerb TV',
+    companyUrl: null,
+    jobPosition: 'Full-Stack Developer',
+    startDate: new Date('August 1, 2019'),
+    endDate: new Date('July 1, 2022'),
+    responsibilities: [
+      { description: 'Designed a microservice to process currency foreign exchange rate estimates' },
+      {
+        description: 'Integrated APIs (ie Google Geocoding, IPAP, IPStack) to track a user\'s geo-location.',
+      },
+      {
+        description: 'Created websocket implementation to synchronize data between the browser and API',
+      },
+    ],
+    technologies_used:
+      'Phoenix, Elixir, React, JavaScript, JSX, mobX, JSON, PostgreSQL, Redis, GCP, Linux, Docker, Git, and Github',
+  },
+  {
+    id: 3,
+    companyName: 'Combatting Poverty in New Mexico',
+    companyUrl: null,
+    jobPosition: 'Website Maintainer & Website Developer',
+    startDate: new Date('January 1, 2020'),
+    endDate: new Date('August 1, 2021'),
+    responsibilities: [
+      { description: 'Recreated template provided by client' },
+      { description: 'Tested for cross browser compatibility' },
+      { description: 'Created staging area for progress reports' },
+    ],
+    technologies_used: 'HTML, CSS, and cPanel',
+  },
+  {
+    id: 2,
+    companyName: 'Xerb TV',
+    companyUrl: null,
+    jobPosition: 'Intern Developer',
+    startDate: new Date('August 1, 2019'),
+    endDate: new Date('December 1, 2020'),
+    responsibilities: [
+      { description: 'Developed UI for admin users to view and update user information' },
+      {
+        description: 'Developed functionality for administrators supported across all major browsers and devices',
+      },
+      {
+        description: 'Learned about application state and how to manage it using libraries like MobX',
+      },
+    ],
+    technologies_used: 'React, JSX, CSS, JavaScript, Linux, Docker, JSON, Git, and Github',
+  },
+  {
+    id: 1,
+    companyName: 'Thai Café & Noodle Treats',
+    companyUrl: 'https://www.thaicafent.net/',
+    jobPosition: 'Website Designer',
+    startDate: new Date('June 1, 2019'),
+    endDate: new Date('February 1, 2020'),
+    responsibilities: [
+      { description: 'Redesigned website for a more modern and responsive look' },
+      { description: 'Integrated WIX database to display restaurant menu items' },
+    ],
+    technologies_used: 'WIX',
+  },
+];
