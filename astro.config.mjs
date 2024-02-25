b7fd74c8741676d6946857c4ac7c0f85cdca8ddd
marioBytes <mario.bytes92@gmail.com>
2024-02-25 16:27:27 -0700

    Constant variables not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-SERVER_PORT = 3000;
-BASE_URL = 'https://mariobytes.github.io';
-
 // https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT },
-  site: BASE_URL,
+  site: 'https://mariobytes.github.io',
 });
