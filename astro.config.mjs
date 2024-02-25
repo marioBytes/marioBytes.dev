90de5db60a7f84591ec10efe27a15c46abd29adc
marioBytes <mario.bytes92@gmail.com>
2024-02-25 16:25:28 -0700

    Resolve PORT not being defined

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-PORT = 3000;
+SERVER_PORT = 3000;
 BASE_URL = 'https://mariobytes.github.io';
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: PORT },
+  server: { port: SERVER_PORT },
   site: BASE_URL,
 });
