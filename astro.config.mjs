a9ddd93462ef8a6e36af8e90238f09176fe12c55
marioBytes <mario.bytes92@gmail.com>
2024-02-25 16:17:12 -0700

    Add https to beginning of base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 PORT = 3000;
-BASE_URL = 'marioBytes.github.io';
+BASE_URL = 'https://marioBytes.github.io';
 
 // https://astro.build/config
 export default defineConfig({
