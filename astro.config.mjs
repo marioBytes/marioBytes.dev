d2a043c55ff84f77f5a1bce956514d87026fcf28
marioBytes <mario.bytes92@gmail.com>
2024-02-25 16:14:56 -0700

    change no longer needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 PORT = 3000;
-BASE_URL = 'mariobytes.github.io';
+BASE_URL = 'marioBytes.github.io';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: PORT },
   site: BASE_URL,
-  base: 'marioBytes.dev/',
 });
